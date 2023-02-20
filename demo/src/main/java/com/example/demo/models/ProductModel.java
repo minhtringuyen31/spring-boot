174320b2d82b68725bae703920793cf1437cb26b
Nguyen Vo Minh Tri <minhtri.nguyenvo31@gmail.com>
2023-02-20 19:41:00 +0700

    main controller

--- demo/src/main/java/com/example/demo/models/ProductModel.java
+++ demo/src/main/java/com/example/demo/models/ProductModel.java
@@ -1,79 +1,79 @@
-package com.example.demo.models;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
-public class ProductModel {
-    //This is "primary key"
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO) //Auto-increment
-    private Long id;
-    private String productName;
-    private int year;
-    private Double price;
-    private String url;
-
-    public ProductModel() {
-    }
-
-    public ProductModel(String productName, int year, Double price, String url) {
-        this.productName = productName;
-        this.year = year;
-        this.price = price;
-        this.url = url;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", productName='" + productName + '\'' +
-                ", year=" + year +
-                ", price=" + price +
-                ", url='" + url + '\'' +
-                '}';
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-}
+//package com.example.demo.models;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//
+//@Entity
+//public class ProductModel {
+//    //This is "primary key"
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO) //Auto-increment
+//    private Long id;
+//    private String productName;
+//    private int year;
+//    private Double price;
+//    private String url;
+//
+//    public ProductModel() {
+//    }
+//
+//    public ProductModel(String productName, int year, Double price, String url) {
+//        this.productName = productName;
+//        this.year = year;
+//        this.price = price;
+//        this.url = url;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Product{" +
+//                "id=" + id +
+//                ", productName='" + productName + '\'' +
+//                ", year=" + year +
+//                ", price=" + price +
+//                ", url='" + url + '\'' +
+//                '}';
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getProductName() {
+//        return productName;
+//    }
+//
+//    public void setProductName(String productName) {
+//        this.productName = productName;
+//    }
+//
+//    public int getYear() {
+//        return year;
+//    }
+//
+//    public void setYear(int year) {
+//        this.year = year;
+//    }
+//
+//    public Double getPrice() {
+//        return price;
+//    }
+//
+//    public void setPrice(Double price) {
+//        this.price = price;
+//    }
+//
+//    public String getUrl() {
+//        return url;
+//    }
+//
+//    public void setUrl(String url) {
+//        this.url = url;
+//    }
+//}
