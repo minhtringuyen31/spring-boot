3cab69768226e563070c9957a3c79509ae15cefb
Nguyen Vo Minh Tri <minhtri.nguyenvo31@gmail.com>
2023-02-21 20:26:38 +0700

    implement users method

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,4 +1,24 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.UserModel;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/users")
+    public String showUserList(Model model){
+        List<UserModel> listUsers = service.listAll();
+        System.out.println(listUsers.toString());
+        model.addAttribute("listUsers", listUsers);
+        return "users";
+    }
 }
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,4 +1,18 @@
 package com.example.demo.services;
 
+import com.example.demo.models.UserModel;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository repo;
+
+    public List<UserModel> listAll(){
+        return (List<UserModel>) repo.findAll();
+    }
 }
